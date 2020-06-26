bfd77371369ddcd203d0b8aca7aa117ea51042fa
Razvan Papadopol <razvan.papadopol@automattic.com>
2020-06-26 18:21:46 +0300

    update block settings to prevent rendering BlockContextualToolbar

--- client/landing/gutenboarding/gutenboard.tsx
+++ client/landing/gutenboarding/gutenboard.tsx
@@ -88,6 +88,7 @@ export function Gutenboard() {
 							settings={ {
 								templateLock: 'all',
 								alignWide: true,
+								hasFixedToolbar: true,
 							} }
 						>
 							<div className="gutenboarding__content edit-post-layout__content">
