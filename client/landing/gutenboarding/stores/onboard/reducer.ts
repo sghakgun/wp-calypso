73810d577e0e14de61ab4ec9dbd96df1f3571273
roo2 <roo2@protonmail.com>
2020-07-06 15:13:20 +1000

    remove siteLanguage for now and just use the current locale when creating the site

--- client/landing/gutenboarding/stores/onboard/reducer.ts
+++ client/landing/gutenboarding/stores/onboard/reducer.ts
@@ -35,13 +35,6 @@ const domainSearch: Reducer< string, OnboardAction > = ( state = '', action ) =>
 	return state;
 };
 
-const siteLanguage: Reducer< string, OnboardAction > = ( state = '', action ) => {
-	if ( action.type === 'SET_SITE_LANGUAGE' ) {
-		return action.siteLanguage;
-	}
-	return state;
-};
-
 const domainCategory: Reducer< string | undefined, OnboardAction > = ( state, action ) => {
 	if ( action.type === 'SET_DOMAIN_CATEGORY' ) {
 		return action.domainCategory;
@@ -164,7 +157,6 @@ const reducer = combineReducers( {
 	siteTitle,
 	siteVertical,
 	showSignupDialog,
-	siteLanguage,
 } );
 
 export type State = ReturnType< typeof reducer >;
